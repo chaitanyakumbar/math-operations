0550e423708069b87dbb43fe83dbbd8dd2070886
chaitanyakumbar <chaitanyakumbar30@gmail.com>
2025-10-23 20:57:54 +0530

    Create mathoperations.py

---
+++ mathoperations.py
@@ -0,0 +1,11 @@
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Addition and subtraction
+sum_result = num1 + num2
+diff_result = num1 - num2
+
+# Display results
+print("Addition:", sum_result)
+print("Subtraction:", diff_result)
